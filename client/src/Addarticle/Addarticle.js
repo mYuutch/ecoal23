7d0ecd44095cec186c81d04249d03c1f2d51a613
Drique Noah <nwyuxtfm@gmail.com>
2023-03-08 12:10:33 +0100

    add article

--- client/src/Addarticle/Addarticle.js
+++ client/src/Addarticle/Addarticle.js
@@ -9,17 +9,31 @@ export default function Addarticle() {
     const [content, setContent] = useState('');
     const [thumbnailURL, setThumbnailURL] = useState('');
     const [token, setUserToken] = useCookie('token', '0');
+    const [mediaType, setMediaType] = useState('');
+    const [mediaURL, setMediaURL] = useState('');
 
     async function addArticle() {
         const response = await axios.post('http://localhost:8000/api/article', {
             title: title,
             content: content,
             thumbnailURL: thumbnailURL,
+            mediaType : mediaType,
+            mediaURL : mediaURL,
+            leadStory: true,
             token: token
         });
         console.log(response);
         window.location.href = '/';
     }
+
+    function changePath() {
+        const file = document.getElementById('thumbnailURL');
+        const path = file.value;
+        const filename = path.replace(/^.*\\/, "/");
+        console.log(filename);
+        setThumbnailURL(filename);
+    }
+
 
     return (
         <div>
@@ -30,8 +44,16 @@ export default function Addarticle() {
                     <input type='text' onChange={(e) => setTitle(e.target.value)} />
                     <h3>Content</h3>
                     <input type='text' onChange={(e) => setContent(e.target.value)} />
-                    <h3>Thumbnail URL</h3>
-                    <input type='text' onChange={(e) => setThumbnailURL(e.target.value)} />
+                    <h3>Thumbnail (only png or jpeg)</h3>
+                    <input type='file'  accept='image/png, image/jpeg' id='thumbnailURL' onChange={changePath} />
+                    <h3>Media Type</h3>
+                    <select onChange={(e) => setMediaType(e.target.value)}>
+                        <option value="image">Image</option>
+                        <option value="video">Video</option>
+                        <option value="audio">Audio</option>
+                    </select>
+                    <h3>Media URL</h3>
+                    <input type='text' onChange={(e) => setMediaURL(e.target.value)} />
                     <button onClick={addArticle}>Add Article</button>
                 </div>
             </div>
