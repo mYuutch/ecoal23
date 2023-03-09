396ad3e45839b08f409a749c85471d9c3ea07329
Drique Noah <nwyuxtfm@gmail.com>
2023-03-09 11:28:21 +0100

    footer

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Menu from './Menu/Menu';
+import Footer from './Footer/Footer';
 
 function App() {
 
   return (
     <div className="App">
       <Menu />
+      <Footer />
     </div>
   );
 }
