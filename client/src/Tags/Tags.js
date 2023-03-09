d83d3b3356c37e88e0c6d2e42558e9700e26c4c2
JulienF777 <juju56260@live.fr>
2023-03-09 11:13:15 +0000

    style tags

--- client/src/Tags/Tags.js
+++ client/src/Tags/Tags.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useParams } from 'react-router-dom';
+import './tags.css';
 
 
 export default function Tags() {
